fbf494fdedc60dedbac7cac6cd078451ac9f880f
veeravel-iyappan-49 <velveeravel4339@gmail.com>
2025-07-18 15:59:15 +0530

    main page added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
+import dark_arrow from "../../assets/dark-arrow.png";
 
 const Hero = () => {
   return (
-    <div className='hero'></div>
-  )
-}
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>The place for a better education for a better WORLD</h1>
+        <p>
+          Whether you're a complete beginner or looking to sharpen your skills,
+          EDUSITY Academy is your launchpad to mastering programming. We offer
+          hands-on lessons, real-world projects, and expert guidance to help you
+          become a confident developer.
+        </p>
+        <button className="btn">
+          Explore More <img src={dark_arrow} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
